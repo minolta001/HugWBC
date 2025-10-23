8f058cda2386f635992c8714bd48417e4c026d3c
Li Chen <lchen0@umass.edu>
2025-10-23 17:39:46 -0400

    update .gitignore and h12wbc

--- legged_gym/scripts/h12_wbc.py
+++ legged_gym/scripts/h12_wbc.py
@@ -5,6 +5,7 @@ from legged_gym import LEGGED_GYM_ROOT_DIR
 import isaacgym
 from legged_gym.envs import *
 from legged_gym.utils import get_args, task_registry, update_class_from_dict
+from legged_gym.utils.low_state_controller import LowStateCmdHandler
 from isaacgym import gymapi
 import numpy as np
 import torch
@@ -12,15 +13,17 @@ import tqdm
 from torch.utils.tensorboard import SummaryWriter
 import yaml
 from isaacgym import gymapi
+from gs_env.real.low_state_controller import LowStateCmdHandler
+import time
 
-def play(args):
+def deploy(args):
     env_cfg, train_cfg = task_registry.get_cfgs(name=args.task)
     resume_path = train_cfg.runner.resume_path
     print(resume_path)
     
     # override some parameters for testing
     env_cfg.env.num_envs = min(env_cfg.env.num_envs, 1)
-    env_cfg.env.episode_length_s = 100000
+    env_cfg.env.episode_projectedlength_s = 100000 
 
     env_cfg.terrain.curriculum = False
     env_cfg.noise.add_noise = True
@@ -48,68 +51,88 @@ def play(args):
             },
     }
 
+    #env_cfg.env.has_privileged_info=False
+
     # prepare     # planeenvironment
     env, _ = task_registry.make_env(name=args.task, args=args, env_cfg=env_cfg)
-    for i in range(env.num_bodies):
-        env.gym.set_rigid_body_color(env.envs[0], env.actor_handles[0], i, gymapi.MESH_VISUAL, gymapi.Vec3(0.3, 0.3, 0.3))
+
+
     # load policy
     train_cfg.runner.resume = True
     ppo_runner, train_cfg = task_registry.make_alg_runner(
         env=env, name=args.task, args=args, train_cfg=train_cfg)
     policy = ppo_runner.get_inference_policy(device=env.device)
 
-    cfg_eval = {
-        "timesteps": (env_cfg.env.episode_length_s) * 500 + 1,
-        'cameraTrack': True, 
-        'trackIndex': 0,
-        "cameraInit": np.pi*8/10,  
-        "cameraVel": 1*np.pi/10,
-    }
-    camera_rot = np.pi * 8 / 10
-    camera_rot_per_sec = 1 * np.pi / 10
-    camera_relative_position = np.array([1, 0, 0.8])
-    track_index = 0
-
-    look_at = np.array(env.root_states[0, :3].cpu(), dtype=np.float64)
-    env.set_camera(look_at + camera_relative_position, look_at, track_index)
-    
+  
     _, _ = env.reset()
+
+    # initialize 
+    actions = torch.zeros(env.num_envs, env.num_actions, dtype=torch.float, device=env.device)
+
     obs, critic_obs, _, _, _ = env.step(torch.zeros(
             env.num_envs, env.num_actions, dtype=torch.float, device=env.device))
+    
+    commands = np.array([
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ])
+    commands[:, 0] = 0
+    commands[:, 1] = 0
+    commands[:, 2] = 0
+    commands[:, 3] = 2.0
+    commands[:, 4] = 0.5
+    commands[:, 5] = 0.5
+    commands[:, 6] = 0.2
+    commands[:, 7] = -0.0
+    commands[:, 8] = 0.0
+    commands[:, 9] = 0.0
+ 
+    cmd_handler = LowStateCmdHandler(cfg)
+    
 
-    timesteps = env_cfg.env.episode_length_s * 500 + 1
+    try:
+        while not 
+
+
+
+
+
     for timestep in tqdm.tqdm(range(timesteps)):
         with torch.inference_mode():
-            #actions, _ = policy.act_inference(obs, privileged_obs=critic_obs)
-            actions, _ = policy.act_inference(obs, privileged_obs=None)
-            obs, critic_obs, _, _, _ = env.step(actions)
-            look_at = np.array(env.root_states[track_index, :3].cpu(), dtype=np.float64)
-            camera_rot = (camera_rot + camera_rot_per_sec * env.dt) % (2 * np.pi)
-            h_scale = 1
-            v_scale = 0.8
-            camera_relative_position = 2 * \
-                np.array([np.cos(camera_rot) * h_scale,
-                         np.sin(camera_rot) * h_scale, 0.5 * v_scale])
-            #env.set_camera(look_at + camera_relative_position, look_at, track_index)
+            actions, _ = policy.act_inference(obs, privileged_obs=critic_obs)
 
-            env.commands[:, 0] = 1.0 #2.0   # x velocity  [-0.6, 2.0]
-            env.commands[:, 1] = 0 # y velocity [-0.6, 0.6]
-            env.commands[:, 2] = 0    # yaw [-1, 1]
-            env.commands[:, 3] = 2      # feet step frequency [1.5, 3.5]    # gait frequency
-            env.commands[:, 4] = 0.5    # 0 jumping, 0.5 waliing, 0.25 mix of jumping and walking
-            env.commands[:, 5] = 0.5    #0.5 feet-ground contact duration. The smaller value, the longer contact time
-            env.commands[:, 6] = 0.2    # foot swing height  [0.1, 0.35]
-            env.commands[:, 7] = -0   # body height [-0.3, 0.3]
-            env.commands[:, 8] = 0    # body pitch [0.0, 0.4] 
-            env.commands[:, 9] = 1.0    # waist roll [-1.0, 1.0]
-            env.use_disturb = False
+            obs, critic_obs, _, _, _ = env.step(actions)
+
+            h_scale = 1
+            v_scale = 0.
+
+
+
+            env.commands[:, 0] = 0
+            env.commands[:, 1] = 0
+            env.commands[:, 2] = 0
+            env.commands[:, 3] = 2.0
+            env.commands[:, 4] = 0.5
+            env.commands[:, 5] = 0.5
+            env.commands[:, 6] = 0.2
+            env.commands[:, 7] = -0.0
+            env.commands[:, 8] = 0.0
+            env.commands[:, 9] = 0.0
+            env.use_disturb = True
             env.disturb_masks[:] = True
             env.disturb_isnoise[:]= True
             env.disturb_rad_curriculum[:] = 1.0
             env.interrupt_mask[:] = env.disturb_masks[:]
-            env.standing_envs_mask[:] = False #True
+            env.standing_envs_mask[:] = True
             env.commands[env.standing_envs_mask, :3] = 0
 
 if __name__ == '__main__':
     args = get_args()
-    play(args)
+    deploy(args)
